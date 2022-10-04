5fa2e6805019a596b20a760af3ba66c62698c1f8
notandy82 <notandystanek@gmail.com>
2022-10-04 11:20:24 +0000

    React lesson 2

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreeting />
+      <FunctionalGreetingWithProps />
+    </div>
+  );
+}
+
+export default App;

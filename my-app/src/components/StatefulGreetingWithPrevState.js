1907cc792742659ea5668fddfac3c098a765bc7e
notandy82 <notandystanek@gmail.com>
2022-10-05 11:53:11 +0000

    Add set state for enter exit button

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -11,22 +11,24 @@ class StatefulGreetingWithPrevState extends React.Component {
     }
 
     handleClick() {
-        this.setState({
-            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-        }, ()=>{
-            console.log('new state', this.state.introduction);
-            console.log('new state', this.state.buttonText);
-        }
-        );
-        console.log(this.state.introduction);
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:' , prevProps)
+            return {
+                introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            }
+        })
     }
 
     increment() {
-        this.setState({
-            count: this.state.count + 1
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                count: prevState.count + 1
+            }
         })
-        console.log(this.state.count)
     }
 
     incrementFive(){
@@ -46,8 +48,8 @@ class StatefulGreetingWithPrevState extends React.Component {
                 <button onClick={() => this.handleClick()}>
                     {this.state.buttonText}
                 </button>
-                <button onClick={() => this.incrementFive()}>
-                    Increment five
+                <button onClick={() => this.increment()}>
+                    Increment
                 </button>
                 <p>You've clicked {this.state.count} times</p>
             </div>            

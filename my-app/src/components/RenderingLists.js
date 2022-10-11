c58fcb9a4e64f3defba908973095f2b6376abfce
notandy82 <notandystanek@gmail.com>
2022-10-11 08:59:13 +0000

    add key to mapping

--- my-app/src/components/RenderingLists.js
+++ my-app/src/components/RenderingLists.js
@@ -28,12 +28,23 @@ function RenderingLists() {
     return (
         <div>
             {bookList.map(book => {
-                return <h2>{book}</h2>
+                return <h2 key={book}>{book}</h2>
             })}
             <hr />
             {
                 books.map(book => {
-                    return <Book book={book} />                    
+                    return (
+                        <div key={book.title}>
+                            <h5>{book.title}</h5>
+                            <p>{book.author}</p>
+                            <p>{book.pages}</p>
+                        </div>
+                    )
+                })
+            }
+            {
+                books.map(book => {
+                    return <Book key={book.title} book={book} />                    
                 })
             }
         </div>

13a557a5c2ba84a3e2876e30f81ad2dd3323a83f
notandy82 <notandystanek@gmail.com>
2022-10-20 13:36:29 +0000

    Api lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import HTTPHooks from "./components/HTTPHooks";
 
-import UseEffectCounterContainer from "./components/UseEffectCounterContainer";
 
 function App() {
   return <div className="App">
-      <UseEffectCounterContainer />
+      <HTTPHooks />
   </div>;
 }
 
